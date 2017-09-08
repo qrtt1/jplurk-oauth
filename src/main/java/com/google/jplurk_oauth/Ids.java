09db71ec6ee04dc300c2587283a6f6374d714d76
Ching Yi, Chan <chingyichan.tw@gmail.com>
2017-09-09 01:47:26 +0800

    idList should be a JSON array #13

--- src/main/java/com/google/jplurk_oauth/Ids.java
+++ src/main/java/com/google/jplurk_oauth/Ids.java
@@ -3,33 +3,23 @@ package com.google.jplurk_oauth;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+
 public class Ids {
 
     private List<Integer> idList = new ArrayList<Integer>();
 
     public Ids() {
     }
-    
+
     public Ids(List<Integer> idList) {
-        if(idList != null && !idList.isEmpty())
-        {
+        if (idList != null && !idList.isEmpty()) {
             this.idList.addAll(idList);
         }
     }
 
     public String formatted() {
-        if (!idList.isEmpty()) {
-            StringBuffer buffer = new StringBuffer();
-            for (Integer id : idList) {
-                buffer.append(id);
-                buffer.append(",");
-            }
-            if (buffer.length() > 0) {
-                buffer.setLength(buffer.length() - 1);
-                return buffer.toString();
-            }
-        }
-        return "[]";
+        return new JSONArray(idList).toString();
     }
 
 }
